ed00838e3d1f2dab88bcf0cb38f50f57fae6cc30
maslowind <bart1885@gmail.com>
2020-06-10 11:29:17 +0300

    demo

---
+++ Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace Graduate_work
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 660);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 29);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "R1/R0";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(118, 29);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "0,3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(118, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "ν";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 74);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(333, 580);
+            this.listBox1.TabIndex = 9;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(351, 74);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(333, 580);
+            this.listBox2.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Чисельний розв\'язок";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(348, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Аналітичний розв\'язок";
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(692, 29);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(384, 302);
+            this.chart1.TabIndex = 13;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.chart2.Legends.Add(legend5);
+            this.chart2.Location = new System.Drawing.Point(1082, 29);
+            this.chart2.Name = "chart2";
+            series5.ChartArea = "ChartArea1";
+            series5.Legend = "Legend1";
+            series5.Name = "Series1";
+            this.chart2.Series.Add(series5);
+            this.chart2.Size = new System.Drawing.Size(384, 302);
+            this.chart2.TabIndex = 14;
+            this.chart2.Text = "chart2";
+            // 
+            // chart3
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chart3.Legends.Add(legend6);
+            this.chart3.Location = new System.Drawing.Point(692, 355);
+            this.chart3.Name = "chart3";
+            series6.ChartArea = "ChartArea1";
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            this.chart3.Series.Add(series6);
+            this.chart3.Size = new System.Drawing.Size(384, 302);
+            this.chart3.TabIndex = 15;
+            this.chart3.Text = "chart3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(689, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(195, 17);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Радіальне напруженняσ_RR";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1079, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 17);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Переміщення s";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(695, 334);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(195, 17);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Кільцьове напруження σ_φφ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(224, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 20;
+            this.textBox1.Text = "2";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(224, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(15, 17);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "γ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1560, 699);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Дипломна робота";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Graduate_work
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Result
+        {
+            public double R { get; set; }
+            public double S_tt { get; set; }
+            public double S_st { get; set; }
+            public double S_ts { get; set; }
+            public double S_ss { get; set; }
+            public double sigma_rr { get; set; }
+            public double u_r { get; set; }
+            public double sigma_fifi { get; set; }
+        }
+        double nu, mu, alpha, beta, gamma;
+        double R0 = 1, R1, R, h;
+        double  tau_coef_R1, A_coef_R1;
+        List<Result> staticResults = new List<Result>();
+        List<Result> results = new List<Result>();
+        List<Result> results_lin = new List<Result>();
+        List<Result> results_quad = new List<Result>();
+        List<Result> results_cube = new List<Result>();
+        Func<double, double> G_proiz;
+        List<Func<double, double>> G_proiz_list = new List<Func<double, double>>();
+        double S_tt_static(double R)
+        {
+            return Math.Pow(R / R0, alpha) * (mu * (1 - gamma / alpha) - (mu + alpha) * Math.Pow(R / R0, beta)) / beta;
+        }
+        double S_st_static(double R)
+        {
+            return mu * Math.Pow(R / R0, alpha) * (Math.Pow(R / R0, beta) - 1) / beta;
+        }
+        double S_ts_static(double R)
+        {
+            return Math.Pow(R / R0, alpha) * (Math.Pow(R / R0, beta) - 1) / (beta * (1 - nu));
+        }
+        double S_ss_static(double R)
+        {
+            return Math.Pow(R / R0, alpha) * (-1 * (mu + alpha) + mu * (1 - gamma / alpha) * Math.Pow(R / R0, beta)) / beta;
+        }
+
+        double G_static(double R)
+        {
+            return Math.Pow(R, gamma);
+        }
+        double G_lin(double R)
+        {
+            return 2 * R + 20;
+        }
+        double G_quad(double R)
+        {
+            return 10 * R * R + 5 * R + 43;
+        }
+        double G_cube(double R)
+        {
+            return R*R*R + 20*R*R + 7;
+        }
+        
+
+
+
+        double ln_G_static_proiz(double R)
+        {
+            return gamma / R;
+        }
+        double ln_G_lin_proiz(double R)
+        {
+            return 2.0 / G_lin(R);
+        }
+        double ln_G_quad_proiz(double R)
+        {
+            return 1.0 * (20 * R + 5) / G_quad(R);
+        }
+        double ln_G_cube_proiz(double R)
+        {
+            return 1.0*(3 * R*R+40*R) / G_cube(R);
+        }
+
+
+        double sigma_rr_func(double R, List<Result> list)
+        {
+            int id = list.FindIndex(x => Math.Round(x.R, 3) == Math.Round(R, 3));
+            return (list[id].S_tt - A_coef_R1 * list[id].S_ts) * tau_coef_R1;
+        }
+        double u_r_func(double R, List<Result> list)
+        {
+            int id = list.FindIndex(x => Math.Round(x.R, 3) == Math.Round(R, 3));
+            return (list[id].S_st - A_coef_R1 * list[id].S_ss) * tau_coef_R1;
+        }
+        double sigma_fifi_func(double R, List<Result> list)
+        {
+            int id = list.FindIndex(x => Math.Round(x.R, 3) == Math.Round(R, 3));
+            return (nu * list[id].sigma_rr+ list[id].u_r)/(1+nu);
+        }
+
+
+
+
+        double sigma_rr_proiz(double R, double sigma_rr, double s)
+        {
+            double result = s / ((1 - nu) * R) - (1 - 2 * nu) * sigma_rr / ((1 - nu) * R);
+            return result;
+        }
+        double s_proiz(double R, double s, double sigma_rr)
+        {
+            double result = (1 - 2 * nu) * sigma_rr / ((1 - nu) * R) - s / ((1 - nu) * R) + s * G_proiz(R);
+            return result;
+        }
+
+        void  getCoef(List<Result> list)
+        {
+            int id = list.FindIndex(x => Math.Round(x.R,2) == Math.Round(R1,2));
+            double a = list[id].S_tt;
+            double b = list[id].S_ts;
+            double c = list[id].S_st;
+            double d = list[id].S_ss;
+            tau_coef_R1 = 1;
+            A_coef_R1 = 1.0*c/d;
+        }
+
+        private double methodRungeKutta(double x, double y1, double y2, double h, bool leftOrRight, Func<double, double, double, double> func)
+        {
+            double k1, k2, k3, k4, koef;
+            if (leftOrRight) koef = 1;
+            else koef = -1;
+            h = h * koef;
+            k1 = h * func(x, y1, y2);
+            k2 = h * func(x + h / 2, y1 + koef * k1 / 2, y2);
+            k3 = h * func(x + h / 2, y1 + koef * k2 / 2, y2);
+            k4 = h * func(x + h, y1 + koef * k3, y2);
+            double result = y1 + koef * (k1 + 2 * k2 + 2 * k3 + k4) / 6;
+            return result;
+        }
+
+        void addFuncToList()
+        {
+            G_proiz_list.Add(new Func<double, double>(ln_G_static_proiz));
+            G_proiz_list.Add(new Func<double, double>(ln_G_lin_proiz));
+            G_proiz_list.Add(new Func<double, double>(ln_G_quad_proiz));
+            G_proiz_list.Add(new Func<double, double>(ln_G_cube_proiz));
+        }
+
+        void addResults(int i, double R, double sigma_rr_1, double s_0, double sigma_rr_0, double s_1,ref List<Result> list)
+        {
+
+            list.Add(new Result());            
+            list[i].R = R;
+            list[i].S_tt = sigma_rr_1;
+            list[i].S_st = s_0;
+            list[i].S_ts = sigma_rr_0;
+            list[i].S_ss = s_1;
+        }
+        void addFinalResults( ref List<Result> list)
+        {
+            for (int j = 0; j < list.Count; j++)
+            {
+                list[j].sigma_rr = sigma_rr_func(list[j].R, list);
+                list[j].u_r = u_r_func(list[j].R, list);
+                list[j].sigma_fifi = sigma_fifi_func(list[j].R, list);
+            }
+        }
+
+        void calculateEverything(ref List<Result> list, string name, Color color)
+        {
+            R = R0;
+            int i = 1;
+            h = 0.05;
+            double sigma_rr_1 = 1, s_0 = 0, sigma_rr_0 = 0, s_1 = 1;
+            addResults(0, R, sigma_rr_1, s_0, sigma_rr_0, s_1, ref list);
+            
+            while (R <= R1)
+            {
+                R += h;
+                double sigma_rr_1_2 = methodRungeKutta(R, sigma_rr_1, s_0, h, true, sigma_rr_proiz);
+                double s_0_2 = methodRungeKutta(R, s_0, sigma_rr_1, h, true, s_proiz);
+                double sigma_rr_0_2 = methodRungeKutta(R, sigma_rr_0, s_1, h, true, sigma_rr_proiz);
+                double s_1_2 = methodRungeKutta(R, s_1, sigma_rr_0, h, true, s_proiz);
+                sigma_rr_1 = sigma_rr_1_2; s_0 = s_0_2; sigma_rr_0 = sigma_rr_0_2; s_1 = s_1_2;
+
+                addResults(i, R, sigma_rr_1, s_0, sigma_rr_0, s_1, ref list);
+                i++;
+            }
+            getCoef(list);
+            addFinalResults(ref list);
+            addToChart(list, name, color);
+        }
+        void addToChart(List<Result> list, string name, Color color)
+        {
+            chart1.ChartAreas[0].AxisX.Maximum = R1; chart1.ChartAreas[0].AxisX.Minimum = R0;
+            chart2.ChartAreas[0].AxisX.Maximum = R1; chart2.ChartAreas[0].AxisX.Minimum = R0;
+            chart3.ChartAreas[0].AxisX.Maximum = R1; chart3.ChartAreas[0].AxisX.Minimum = R0;
+            Random rnd = new Random();
+            Series func_sigma_rr = new Series(name);
+            Series func_u_r = new Series(name);
+            Series func_sigma_fifi = new Series(name);
+            func_sigma_rr.ChartType = SeriesChartType.Line;
+            func_sigma_rr.Color = color;
+            func_u_r.ChartType = SeriesChartType.Line;
+            func_u_r.Color = color;
+            func_sigma_fifi.ChartType = SeriesChartType.Line;
+            func_sigma_fifi.Color = color;
+            for (int i = 0; i < list.Count; i++)
+            {
+                func_sigma_rr.Points.AddXY(list[i].R, list[i].sigma_rr);
+                func_u_r.Points.AddXY(list[i].R, list[i].u_r);
+                func_sigma_fifi.Points.AddXY(list[i].R, list[i].sigma_fifi);
+            }
+            chart1.Series.Add(func_sigma_rr);
+            chart2.Series.Add(func_u_r);
+            chart3.Series.Add(func_sigma_fifi);
+        }
+        void Reset ()
+        {
+            staticResults.Clear();
+            results.Clear();
+            results_lin.Clear();
+            results_quad.Clear();
+            results_cube.Clear();
+            listBox1.Items.Clear();
+            listBox1.Items.Add("R\t\nS_ττ\t\nS_sτ\t\nS_τs\t\nS_ss");
+            listBox2.Items.Clear();
+            listBox2.Items.Add("R\t\nS_ττ\t\nS_sτ\t\nS_τs\t\nS_ss");
+            while (chart1.Series.Count > 0) { chart1.Series.RemoveAt(0); }
+            while (chart2.Series.Count > 0) { chart2.Series.RemoveAt(0); }
+            while (chart3.Series.Count > 0) { chart3.Series.RemoveAt(0); }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Reset();            
+            R1 = Convert.ToDouble(textBox2.Text);
+            nu = Convert.ToDouble(textBox3.Text);
+            gamma = Convert.ToDouble(textBox1.Text);
+            mu = 1.0 * (1 - 2 * nu) / (1 - nu);
+            beta = Math.Sqrt(gamma * gamma - (4.0 * nu * gamma) / (1 - nu) + 4);
+            alpha = 0.5 * (gamma - beta) - 1;
+
+            addFuncToList();
+            G_proiz = G_proiz_list[0];
+            calculateEverything(ref results, "Степенева", Color.FromName("Red"));
+            G_proiz = G_proiz_list[1];
+            calculateEverything(ref results_lin, "Лінійна", Color.FromName("Blue"));
+            G_proiz = G_proiz_list[2];
+            calculateEverything(ref results_quad, "Квадратична", Color.FromName("Green"));
+            G_proiz = G_proiz_list[3];
+            calculateEverything(ref results_cube, "Кубічна", Color.FromName("Black"));
+            R = R0;
+            h = 0.05;
+            for (int i = 0; i < results.Count; i++)
+            {
+
+                addResults(i, R, S_tt_static(R), S_st_static(R), S_ts_static(R), S_ss_static(R), ref staticResults);
+                listBox1.Items.Add(Math.Round(results[i].R, 3) + "\t\n" + Math.Round(results[i].S_tt, 3) + "\t\n" + Math.Round(results[i].S_st, 3) + "\t\n" + Math.Round(results[i].S_ts, 3) + "\t\n" + Math.Round(results[i].S_ss, 3) + "\t\n");
+                listBox2.Items.Add(Math.Round(staticResults[i].R, 3) + "\t\n" + Math.Round(staticResults[i].S_tt, 3) + "\t\n" + Math.Round(staticResults[i].S_st, 3) + "\t\n" + Math.Round(staticResults[i].S_ts, 3) + "\t\n" + Math.Round(staticResults[i].S_ss, 3) + "\t\n");
+                
+                R += h;
+            }
+            
+        }
+    }
+}
